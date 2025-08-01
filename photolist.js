bdea3911b7442e28cf2412ec462ce305ccfeb5da
uranainoyakata <gomino20031028@gmail.com>
2025-08-01 16:40:32 +0900

    Create photolist.js

---
+++ photolist.js
@@ -0,0 +1,25 @@
+// ▼▼▼ このファイルに写真のリストをまとめて管理します ▼▼▼
+// ジャンル名や写真のパスを自由に追加・編集してください。
+// 編集したら、このファイルをGitHubに保存するだけでOKです。
+
+const imageData = {
+    "歴史": [
+        // "images/history/" フォルダに入れた画像を指定
+        "images/history/sengoku.jpg",
+        "images/history/edo.png"
+    ],
+    "科学": [
+        // "images/science/" フォルダに入れた画像を指定
+        "images/science/dna.jpg",
+        "images/science/newton.png"
+    ],
+    "美術": [
+        // "images/art/" フォルダに入れた画像を指定
+        "images/art/monalisa.jpg",
+        "images/art/gogh.png"
+    ]
+    // 例: 新しいジャンルを追加する場合
+    // ,"地理": [
+    //     "images/geography/mt_fuji.jpg"
+    // ]
+};
